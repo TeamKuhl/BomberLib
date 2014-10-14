f53e8204cd106ccf441e5b54110242429a0ebbe0
H3adsho0ot <niklas.roll123@gmail.com>
2014-10-14 08:23:55 +0200

    Deleted old stuff

---
+++ BomberLib/BomberLib/Config.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomberLib
+{
+    public class Config
+    {
+        public Dictionary<String, String> settings = new Dictionary<string, string>();
+
+        /// <summary>
+        ///     loads the config from file 
+        /// </summary>
+        public Config()
+        {
+            // this.initialize();
+        }
+
+
+
+        /// <summary>
+        ///     set a setting
+        /// </summary>
+        /// <param name="name">name of setting</param>
+        /// <param name="value">value of setting</param>
+        public void set(string name, string value)
+        {
+            if (settings.ContainsKey(name))
+            {
+                settings[name] = value;
+            }
+            else
+            {
+                settings.Add(name, value);
+            }
+        }
+
+        /// <summary>
+        ///     get a setting
+        /// </summary>
+        /// <param name="name">name of setting</param>
+        /// <returns></returns>
+        public string get(string name)
+        {
+            if (settings.ContainsKey(name))
+            {
+                if (settings[name].Trim() == "")
+                {
+                    return settings[name].Trim();
+                }
+                else return null;
+            }
+            else return null;
+        }
+    }
+}
---
+++ BomberLib/BomberLib/game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunicationLibrary;
+
+namespace BomberLib
+{
+    class Game
+    {
+        
+
+        public Boolean start()
+        {
+            // load config
+            Config config = new Config();
+
+            // load map
+            BomberMap bomberMap = new BomberMap();
+
+            // create server
+            Server server = new Server();
+
+            // start server
+            server.start(Convert.ToInt32(config.get("server-port")));
+
+            return false;
+        }
+    }
+}
